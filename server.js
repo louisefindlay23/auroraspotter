029048aac970572f1c67fb1471a3991b352eac81
louisefindlay23 <louisefindlay23@outlook.com>
2020-04-03 20:37:14 +0100

    Changed HTML files to EJS and adjusted server.js

--- server.js
+++ server.js
@@ -1,6 +1,44 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('final'));
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function (req, res) {
+    res.render('pages/index');
+});
+
+// change password page
+
+app.get('/change-password', function (req, res) {
+    res.render('pages/change-password');
+});
+
+// login page
+
+app.get('/login', function (req, res) {
+    res.render('pages/login');
+});
+
+// profile page
+
+app.get('/profile', function (req, res) {
+    res.render('pages/profile');
+});
+
+// settings page
+
+app.get('/settings', function (req, res) {
+    res.render('pages/settings');
+});
+
+// change password page
+
+app.get('/signup', function (req, res) {
+    res.render('pages/signup');
+});
 
 app.listen(8080);
