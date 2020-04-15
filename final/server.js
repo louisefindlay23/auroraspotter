e18cad2dc897908faef88c8fe51e7edc3fa82ff0
Muhayyo <muhayyo@hotmail.com>
2020-04-15 13:30:14 +0100

    setup mongodb

--- final/server.js
+++ final/server.js
@@ -1,20 +1,39 @@
-var express = require('express');
-var app = express();
+
+//**************** DATABASE and EXPRESS connections **********************/
+const MongoClient = require('mongodb').MongoClient; 
+const url = "mongodb://localhost:27017/profiles";
+const express = require('express');     // variables to link express
+const app = express();                  // access express functions
+const session = require('express-session');
+const bodyParser = require('body-parser');
 
 app.use(express.static('public'));
+app.use(bodyParser.json());  
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.set('view engine', 'ejs');      // set the view engine to ejs
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
+var db;
 
-// use res.render to load up an ejs view file
-
-// index page
-app.get('/', function (req, res) {
-    res.render('pages/index');
+// connecting to variable db to database
+MongoClient.connect(url, function(err, database) {
+    if (err) throw err;
+    db = database;
+    app.listen(8080);
+    console.log('listening on 8080');
 });
 
-// change password page
 
+//******************** GET ROUTES - display pages **************************/
+
+// root route
+app.get('/', function (req, res) {
+    res.render('pages/index');
+    res.send("Testing the route");
+});
+
+// change password page route
 app.get('/change-password', function (req, res) {
     res.render('pages/change-password');
 });
@@ -43,4 +62,4 @@ app.get('/signup', function (req, res) {
     res.render('pages/signup');
 });
 
-app.listen(8080);
+//app.listen(8080);
