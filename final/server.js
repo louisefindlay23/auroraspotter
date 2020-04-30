f1f331ef560256d9970ee3b81f5e68cdc287bb24
mijamka <1904953@rgu.ac.uk>
2020-04-30 20:56:54 +0100

    prevent user from accessing pages if not logged in

--- final/server.js
+++ final/server.js
@@ -82,6 +82,7 @@ app.get('/', function (req, res) {
 
 // change password route
 app.get('/change-password', function (req, res) {
+     if(!req.session.loggedin){res.redirect('/login');return;}
     var isLogged = req.session.loggedin;
     var msg = '';
     res.render('pages/change-password', {changepass_error: msg, isLoggedIn: isLogged});
@@ -100,7 +101,7 @@ app.get('/login', function (req, res) {
 
 // profile route
 app.get('/profile', function (req, res) {
-
+     if(!req.session.loggedin){res.redirect('/login');return;}
     //Login status
     var isLogged = req.session.loggedin;
     var loggedUser = req.session.username;
@@ -144,41 +145,10 @@ app.get('/profile', function (req, res) {
       });
 
 
-// save observation on database  
-app.post('/put-marker', upload.single('observation'), function (req, res, next) {
-    var isLogged = req.session.loggedin;
-    var error_msg = '';
-    // check if user logged in
-    if (!isLogged) {      
-      error_msg = "Please login to tag the location";
-              res.render('pages/login', {
-                       tagging_error: error_msg,
-                        isLoggedIn: isLogged
-                   }); return;
-    } else {
-      // get location tag variables
-      var username = req.body.username;
-      var date = req.body.date;
-      var time = req.body.time;
-      var latitude = req.body.latitude;
-      var longitude = req.body.longitude;
-      var observation_photo = req.body.observation_photo;
-      
-      db.collection('observations').updateOne(({"username": username}), ({$set: { "date": date,
-                                                                                  "time": time,
-                                                                                   "latitude": latitude,
-                                                                                  "longitude": longitude,
-                                                                                  "observation_photo": observation_photo} })), function(err, result){
-            if(err) throw err; 
-               req.session.loggedin = false;
-               res.redirect('/');
-              }
-        }
-});
-
-
 // settings route
 app.get('/settings', function (req, res) {
+     if(!req.session.loggedin){res.redirect('/login');return;}
+
     var isLogged = req.session.loggedin;
     res.render('pages/settings', {isLoggedIn: isLogged});
 });
