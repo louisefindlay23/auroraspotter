8efee8602666393331828707663bd8513cb9e527
Brandon Reid <60351952+brandonreid11@users.noreply.github.com>
2020-05-01 12:36:49 +0100

    updated default image name.

--- final/server.js
+++ final/server.js
@@ -208,7 +208,7 @@ app.post('/upload-aurora', upload.single('aurora'), function (req, res, next) {
     
         if(!photofile){
     console.log('nophoto');
-            photo_path = 'Default_Image.jpg';
+            photo_path = 'aurora-default.jpg';
     }
     
     else{
